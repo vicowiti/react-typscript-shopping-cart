6ae812c13cb1fd6c17cb8579cba3416f29a1dab9
Victor Owiti <viowiti12@gmail.com>
2022-06-26 01:03:14 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container>
+    </>
+  );
+};
+
+export default App;
