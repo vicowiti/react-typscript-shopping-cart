fd9d855373bb9c6ad0476525484cc83b2702f5e3
Victor Owiti <viowiti12@gmail.com>
2022-06-26 15:30:09 +0300

    added the UI components pending functionality

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { formatCurrency } from "../utils/formatCurrency";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  const quantity = 1;
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-baseline mb-4">
+          <span>{name}</span>
+          <span className="text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add to cart</Button>
+          ) : (
+            <div
+              className="d-flex flex-column align-items-center"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex justify-content-center align-items-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span>
+                  in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default StoreItem;
